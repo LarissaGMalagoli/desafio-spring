cc62976bda81171d6afa180fe96a255aa73d57ef
Joao Victor Magalhaes <joao.jmagalhaes@mercadolivre.com>
2021-07-06 15:50:25 -0300

    Initial commit

---
+++ src/test/java/br/com/grupo3/socialmeli/SocialmeliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupo3.socialmeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmeliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
