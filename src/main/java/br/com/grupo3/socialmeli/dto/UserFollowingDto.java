cb6170ac926dd389a3daa13cd06f223b613cf32c
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-07 12:39:28 -0300

    Update: was updated the structure of DTO and removed imports  not used.

--- src/main/java/br/com/grupo3/socialmeli/dto/UserFollowingDto.java
+++ src/main/java/br/com/grupo3/socialmeli/dto/UserFollowingDto.java
@@ -1,10 +1,40 @@
 package br.com.grupo3.socialmeli.dto;
 
-import br.com.grupo3.socialmeli.model.Seller;
-
-import javax.persistence.OneToMany;
 import java.util.List;
 
 public class UserFollowingDto {
 
+    private Long userId;
+    private String userName;
+    private List<SellerFollowedDto> sellerDtos;
+
+    public UserFollowingDto(Long userId, String userName, List<SellerFollowedDto> sellerDtos) {
+        this.userId = userId;
+        this.userName = userName;
+        this.sellerDtos = sellerDtos;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<SellerFollowedDto> getSellerDtos() {
+        return sellerDtos;
+    }
+
+    public void setSellerDtos(List<SellerFollowedDto> sellerDtos) {
+        this.sellerDtos = sellerDtos;
+    }
 }
