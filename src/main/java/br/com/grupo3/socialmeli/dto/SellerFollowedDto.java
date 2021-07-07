81e394e5a077790f3ea07361d7db3a9eed041425
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-07 15:33:28 -0300

    Update: was removed methods set.

--- src/main/java/br/com/grupo3/socialmeli/dto/SellerFollowedDto.java
+++ src/main/java/br/com/grupo3/socialmeli/dto/SellerFollowedDto.java
@@ -17,16 +17,8 @@ public class SellerFollowedDto {
         return userId;
     }
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
     public String getUserName() {
         return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     @Override
--- src/main/java/br/com/grupo3/socialmeli/dto/UserFollowingDto.java
+++ src/main/java/br/com/grupo3/socialmeli/dto/UserFollowingDto.java
@@ -18,23 +18,11 @@ public class UserFollowingDto {
         return userId;
     }
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public List<SellerFollowedDto> getFollowed() {
         return followed;
-    }
-
-    public void setFollowed(List<SellerFollowedDto> followed) {
-        this.followed = followed;
     }
 }
