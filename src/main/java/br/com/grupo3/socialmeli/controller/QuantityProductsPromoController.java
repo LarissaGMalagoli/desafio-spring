97fbf69b2c9c0adc3135465313941992b44e664c
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-07 18:39:11 -0300

    Fix: was fixed new return using response entity and new type of parameter

--- src/main/java/br/com/grupo3/socialmeli/controller/QuantityProductsPromoController.java
+++ src/main/java/br/com/grupo3/socialmeli/controller/QuantityProductsPromoController.java
@@ -2,13 +2,11 @@ package br.com.grupo3.socialmeli.controller;
 
 import br.com.grupo3.socialmeli.dto.QuantityProductPromoDto;
 import br.com.grupo3.socialmeli.service.QuantityProductPromoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/products/")
 public class QuantityProductsPromoController {
 
     final QuantityProductPromoService quantityProductPromoService;
@@ -17,8 +15,8 @@ public class QuantityProductsPromoController {
         this.quantityProductPromoService = quantityProductPromoService;
     }
 
-    @GetMapping("/{userID}/countPromo/")
-    public QuantityProductPromoDto getQuantityProducts(@RequestParam Long userID) {
-        return quantityProductPromoService.getQuantityProductsPromo(userID);
+    @GetMapping("{userID}/countPromo")
+    public ResponseEntity<QuantityProductPromoDto> getQuantityProducts(@PathVariable Long userID) {
+        return ResponseEntity.ok(quantityProductPromoService.getQuantityProductsPromo(userID));
     }
 }
