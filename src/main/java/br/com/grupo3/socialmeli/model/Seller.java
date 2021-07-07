192eb19431752100e9ed5f627230ddfba7f7521d
LarissaGMalagoli <larissa.malagoli@mercadolivre.com>
2021-07-07 11:45:14 -0300

    feat(models): updating lists for User and Seller

--- src/main/java/br/com/grupo3/socialmeli/model/Seller.java
+++ src/main/java/br/com/grupo3/socialmeli/model/Seller.java
@@ -1,6 +1,7 @@
 package br.com.grupo3.socialmeli.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -8,9 +9,9 @@ public class Seller extends Person{
 
     private int followersCount;
     @OneToMany
-    private List<Post> postList;
+    private List<Post> postList = new ArrayList<>();
     @ManyToMany
-    private List<User> followers;
+    private List<User> followers = new ArrayList<>();
 
     public Seller() {
     }
@@ -39,4 +40,6 @@ public class Seller extends Person{
     public void setFollowers(List<User> followers) {
         this.followers = followers;
     }
+
+
 }
--- src/main/java/br/com/grupo3/socialmeli/model/User.java
+++ src/main/java/br/com/grupo3/socialmeli/model/User.java
@@ -1,13 +1,14 @@
 package br.com.grupo3.socialmeli.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class User extends Person{
 
     @OneToMany
-    private List<Seller> following;
+    private List<Seller> following = new ArrayList<>();
 
     public User() {
 
