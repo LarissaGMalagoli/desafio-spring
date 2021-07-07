b93bee6b20b7d9c6ea788923d5aa0988b3194f78
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-07 12:40:01 -0300

    Update: was updated removed imports not used.

--- src/main/java/br/com/grupo3/socialmeli/controller/UserFollowingController.java
+++ src/main/java/br/com/grupo3/socialmeli/controller/UserFollowingController.java
@@ -1,7 +1,6 @@
 package br.com.grupo3.socialmeli.controller;
 
 import br.com.grupo3.socialmeli.dto.UserFollowingDto;
-import br.com.grupo3.socialmeli.model.User;
 import br.com.grupo3.socialmeli.service.UserFollowingService;
 import javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users/")
