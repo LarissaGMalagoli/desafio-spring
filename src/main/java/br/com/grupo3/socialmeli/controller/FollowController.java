46a737efa127a4aa57a5897a9e65f63ce3254b15
pedro-cattel <pedro.cattel@mercadolivre.com>
2021-07-07 12:27:54 -0300

    Updating followUser. - Method now is using FollowService

--- src/main/java/br/com/grupo3/socialmeli/controller/FollowController.java
+++ src/main/java/br/com/grupo3/socialmeli/controller/FollowController.java
@@ -5,16 +5,15 @@ import br.com.grupo3.socialmeli.model.Seller;
 import br.com.grupo3.socialmeli.model.User;
 import br.com.grupo3.socialmeli.repository.SellerRepository;
 import br.com.grupo3.socialmeli.repository.UserRepository;
+import br.com.grupo3.socialmeli.service.FollowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Optional;
 
 
@@ -25,21 +24,24 @@ public class FollowController {
     SellerRepository sellerRepository;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    FollowService followService;
 
     @Transactional
     @PostMapping("/users/{userId}/follow/{userIdToFollow}}")
     public ResponseEntity<UserDto> followUser(@PathVariable("userId") Long userId, @PathVariable("userIdToFollow") Long userIdToFollow, UriComponentsBuilder uriBuilder ){
+
         Optional<User> optUsuario = userRepository.findById(userId);
         Optional<Seller> optVendedor = sellerRepository.findById(userIdToFollow);
+
         if (optUsuario.isEmpty() && optVendedor.isEmpty()){
             return ResponseEntity.notFound().build();
         }
+
         User usuario = userRepository.getById(userId);
         Seller vendedor = sellerRepository.getById(userIdToFollow);
-        List<User> listaFollowers = vendedor.getFollowers();
-        List<Seller> listaFollowed = usuario.getFollowing();
-        listaFollowers.add(usuario);
-        listaFollowed.add(vendedor);
+
+        followService.follow(usuario,vendedor);
         return ResponseEntity.ok().body(new UserDto(usuario));
     }
 }
