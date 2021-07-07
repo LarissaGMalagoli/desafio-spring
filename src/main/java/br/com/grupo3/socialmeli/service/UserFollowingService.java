c8f728651b98f2303cf6f8156258f325a84d9604
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-07 12:58:48 -0300

    update: was added UserService and removed UserRepository for obtain of a user.

--- src/main/java/br/com/grupo3/socialmeli/service/UserFollowingService.java
+++ src/main/java/br/com/grupo3/socialmeli/service/UserFollowingService.java
@@ -3,37 +3,28 @@ package br.com.grupo3.socialmeli.service;
 import br.com.grupo3.socialmeli.dto.SellerFollowedDto;
 import br.com.grupo3.socialmeli.dto.UserFollowingDto;
 import br.com.grupo3.socialmeli.model.User;
-import br.com.grupo3.socialmeli.repository.UserRepository;
-import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class UserFollowingService {
 
-    final UserRepository userRepository;
+    final UserService userService;
 
-    public UserFollowingService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserFollowingService(UserService userService) {
+        this.userService = userService;
     }
 
-    public UserFollowingDto getSellerFollowing(Long id) throws NotFoundException {
+    public UserFollowingDto getSellerFollowing(Long id) {
+        User user = userService.getById(id);
 
-        Optional<User> user = userRepository.findById(id);
-
-        if (user.isEmpty()) {
-            throw new NotFoundException("User not found");
-        }
-
-        List<SellerFollowedDto> sellerFollowedDto = user.get().getFollowing()
+        List<SellerFollowedDto> sellerFollowedDto = user.getFollowing()
                 .stream()
                 .map(SellerFollowedDto::new)
                 .collect(Collectors.toList());
 
-        return new UserFollowingDto(user.get().getUserId(), user.get().getUserName(), sellerFollowedDto);
-
+        return new UserFollowingDto(user.getUserId(), user.getUserName(), sellerFollowedDto);
     }
 }
