9c16d1ea1a3254af20df9a480f42839d00892d50
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-07 11:50:25 -0300

    Add: was created dto to return the request

---
+++ src/main/java/br/com/grupo3/socialmeli/dto/UserFollowingDto.java
@@ -0,0 +1,10 @@
+package br.com.grupo3.socialmeli.dto;
+
+import br.com.grupo3.socialmeli.model.Seller;
+
+import javax.persistence.OneToMany;
+import java.util.List;
+
+public class UserFollowingDto {
+
+}
