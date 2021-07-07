8f0b58f0b95a41d9f4b3d501d1efea0c958c3615
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-07 19:12:09 -0300

    Update: was updated dto class to receive a post class.

--- src/main/java/br/com/grupo3/socialmeli/dto/PostPromoDto.java
+++ src/main/java/br/com/grupo3/socialmeli/dto/PostPromoDto.java
@@ -1,5 +1,54 @@
 package br.com.grupo3.socialmeli.dto;
 
-public class PostPromoDto {
+import br.com.grupo3.socialmeli.model.Post;
+import br.com.grupo3.socialmeli.model.Product;
 
+import java.time.LocalDate;
+
+public class PostPromoDto {
+    private Long postId;
+    private Product product;
+    private int category;
+    private double price;
+    private boolean hasPromo;
+    private double discount;
+    private LocalDate date = LocalDate.now();
+
+    public PostPromoDto(Post post) {
+        this.postId = post.getPostId();
+        this.product = post.getProduct();
+        this.category = post.getCategory();
+        this.price = post.getPrice();
+        this.hasPromo = post.isHasPromo();
+        this.discount = post.getDiscount();
+        this.date = post.getDate();
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isHasPromo() {
+        return hasPromo;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
 }
