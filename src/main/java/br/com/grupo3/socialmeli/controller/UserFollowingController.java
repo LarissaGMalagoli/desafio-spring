7500153ba1bc940010fd1b32075a61644430c044
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-07 12:34:32 -0300

    Update: was updated the endpoint to added exception and return DTO.

--- src/main/java/br/com/grupo3/socialmeli/controller/UserFollowingController.java
+++ src/main/java/br/com/grupo3/socialmeli/controller/UserFollowingController.java
@@ -2,6 +2,9 @@ package br.com.grupo3.socialmeli.controller;
 
 import br.com.grupo3.socialmeli.dto.UserFollowingDto;
 import br.com.grupo3.socialmeli.model.User;
+import br.com.grupo3.socialmeli.service.UserFollowingService;
+import javassist.NotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +16,15 @@ import java.util.List;
 @RequestMapping("/users/")
 public class UserFollowingController {
 
-    @GetMapping("{UserID}/followed/list")
-    public List<User> getSellers(@PathVariable String UserID) {
-        UserFollowingDto userFollowingDto = new UserFollowingDto();
-        return null;
+    final UserFollowingService userFollowingService;
+
+    public UserFollowingController(UserFollowingService userFollowingService) {
+        this.userFollowingService = userFollowingService;
+    }
+
+    @GetMapping("{userID}/followed/list")
+    public ResponseEntity<UserFollowingDto> getSellers(@PathVariable Long userID) throws NotFoundException {
+
+        return ResponseEntity.ok(userFollowingService.getSellerFollowing(userID));
     }
 }
