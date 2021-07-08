41d7e08761ceb71de0f519b63e3121d31b216adf
wagner negrao <wagnernegrao@mercadolivre.com>
2021-07-08 11:27:03 -0300

    Update: was update method to return list of seller ordered.

--- src/main/java/br/com/grupo3/socialmeli/controller/UserFollowingController.java
+++ src/main/java/br/com/grupo3/socialmeli/controller/UserFollowingController.java
@@ -1,19 +1,9 @@
 package br.com.grupo3.socialmeli.controller;
 
 import br.com.grupo3.socialmeli.dto.UserFollowingDto;
-import br.com.grupo3.socialmeli.model.Seller;
-import br.com.grupo3.socialmeli.model.User;
-import br.com.grupo3.socialmeli.repository.SellerRepository;
-import br.com.grupo3.socialmeli.repository.UserRepository;
 import br.com.grupo3.socialmeli.service.UserFollowingService;
-import javassist.NotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users/")
@@ -26,7 +16,8 @@ public class UserFollowingController {
     }
 
     @GetMapping("{userID}/followed/list")
-    public ResponseEntity<UserFollowingDto> getSellers(@PathVariable Long userID) {
-        return ResponseEntity.ok(userFollowingService.getSellerFollowing(userID));
+    public ResponseEntity<UserFollowingDto> getSellers(@PathVariable Long userID,
+                                                       @RequestParam(required = false) String order) {
+        return ResponseEntity.ok(userFollowingService.getSellerFollowing(userID, order));
     }
 }
